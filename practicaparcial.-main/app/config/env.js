4138c0f61c65647246ae05c0c149a6eb1ebdf6af
kavides7 <kvidesg@miumg.edu.gt>
2024-10-02 18:38:51 -0600

    1

--- practicaparcial.-main/app/config/env.js
+++ practicaparcial.-main/app/config/env.js
@@ -1,5 +1,5 @@
 const env = {
-  database: 'umg_antigua_2024_20404_9ug9',
+  database: 'umg_antigua_2024_20404_9ug',
   username: 'umg_antigua_2024_20404_9ug9_user',
   password: 'jjOk5Ydxvsh0OSqZKcHik74DgZZhAz7t',
   host: 'dpg-crus5qqj1k6c73ff03fg-a.oregon-postgres.render.com',
