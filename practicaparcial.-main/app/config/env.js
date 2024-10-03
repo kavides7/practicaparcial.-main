8e9a9b129867511e0db2b4f81f408c8e4d6b8eb7
kavides7 <kvidesg@miumg.edu.gt>
2024-10-02 18:27:29 -0600

    crear

---
+++ practicaparcial.-main/app/config/env.js
@@ -0,0 +1,15 @@
+const env = {
+  database: 'umg_antigua_2024_20404_9ug9',
+  username: 'umg_antigua_2024_20404_9ug9_user',
+  password: 'jjOk5Ydxvsh0OSqZKcHik74DgZZhAz7t',
+  host: 'dpg-crus5qqj1k6c73ff03fg-a.oregon-postgres.render.com',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+
+module.exports = env;
